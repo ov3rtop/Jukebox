7216851cb78ae8e6bf139ae55bef0e32b981745e
JayJay <31519663+ov3rtop@users.noreply.github.com>
2025-12-06 11:11:45 +0100

    🔙 Rollback: jQuery zurück auf 1.12.4
    Bootstrap 3 unterstützt nur jQuery 1.9.1 - 2.x, NICHT jQuery 3.x
    jQuery Migrate hilft hier nicht - Bootstrap prüft die Version explizit
    
    Für jQuery 3 müsste auch Bootstrap auf Version 4 oder 5 aktualisiert werden

--- htdocs/func.php
+++ htdocs/func.php
@@ -30,8 +30,8 @@ function html_bootstrap3_createHeader($lang="en",$title="Welcome",$url_absolute=
         <!--link rel=\"stylesheet\" href=\"".$url_absolute."_assets/css/viewTree.css\"-->
 
         <!-- Latest compiled and minified JavaScript -->
-        <script src=\"".$url_absolute."_assets/js/jquery-3.7.1.min.js\"></script>
-        <script src=\"".$url_absolute."_assets/js/jquery-migrate-3.4.1.min.js\"></script>
+        <!-- Note: Bootstrap 3 requires jQuery 1.9.1 - 2.x, NOT jQuery 3.x -->
+        <script src=\"".$url_absolute."_assets/js/jquery.1.12.4.min.js\"></script>
         <script src=\"".$url_absolute."_assets/bootstrap-3/js/bootstrap.min.js\"></script>
         <script src=\"".$url_absolute."_assets/bootstrap-3/js/collapse.js\"></script>
         <script src=\"".$url_absolute."_assets/bootstrap-3/js/transition.js\"></script>
